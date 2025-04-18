54955fd9f4f47c4f652c187678276865adaaf7a6
OskarCreate <oskar_morales_f@outlook.com>
2025-04-18 17:32:03 -0500

    Session y cookies

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using SweetNela.Models;
 
 namespace SweetNela.Controllers;
@@ -7,25 +8,56 @@ namespace SweetNela.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
+
+    [TempData]
+    public string Message { get; set; } = string.Empty;
 
     public IActionResult Index()
     {
+        var cookieOption = new CookieOptions
+        {
+            Expires = DateTimeOffset.UtcNow.AddDays(7),
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Strict
+        };
+        Response.Cookies.Append("MiCookie", "ValorDelLaCookie", cookieOption);
         return View();
     }
 
-    public IActionResult Privacy()
+    public async Task<IActionResult> Privacy()
     {
-        return View();
-    }
+        if (User.Identity.IsAuthenticated)
+        {
+            var userId = _userManager.GetUserId(User);
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user != null)
+            {
+                var phoneNumber = user.PhoneNumber ?? "No phone number provided";
+                var email = user.Email;
+                var userName = user.UserName;
+
+                Message = $"Customer {userName} (Email: {email}, Phone: {phoneNumber}) added.";
+            }
+            else
+            {
+                Message = "User data could not be retrieved.";
+            }
+        }
+        else
+        {
+            Message = "No customer is logged in.";
+        }
+
+        return View();
     }
 }
