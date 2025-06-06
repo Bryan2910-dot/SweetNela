d0e228f93aaba372b8c4c2cfabb5aa796b73474a
OskarCreate <oskar_morales_f@outlook.com>
2025-06-06 00:00:01 -0500

    Aplicando arreglo del error

--- Data/Migrations/20250603045015_nuevaposMigracion.cs
+++ Data/Migrations/20250603045015_nuevaposMigracion.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SweetNela.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class nuevaposMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
